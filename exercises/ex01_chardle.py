fe827a02f567694ae92906fe63e827f3021f031d
Justin Vilarino <justinv@ad.unc.edu>
2023-09-13 00:50:31 -0400

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,108 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730718451"
+
+five_letter_word: str = input("Enter a 5-character word: ")
+
+if len(five_letter_word) != 5:
+    print("Error: Word must contain 5 letters")
+    exit()
+
+one_letter: str = input("Enter a single character: ")
+
+if len(one_letter) != 1:
+    print("Error: Must be a single letter")
+    exit()
+
+if len(one_letter) == 1:
+        print("Searching for " + str(one_letter) + " in " + str(five_letter_word))
+
+if one_letter == five_letter_word [0]:
+    print(str(one_letter) + " found at index 0")
+
+if one_letter == five_letter_word [1]:
+    print(str(one_letter) + " found at index 1")
+
+if one_letter == five_letter_word [2]:
+    print(str(one_letter) + " found at index 2")
+
+if one_letter == five_letter_word [3]:
+    print(str(one_letter) + " found at index 3")
+
+if one_letter == five_letter_word [4]:
+    print(str(one_letter) + " found at index 4")
+                    
+if (one_letter == five_letter_word [0]) == 0:
+     print("No instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [0]) == 1:
+     print("1 instance of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [0]) == 2:
+     print("2 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [0]) == 3:
+     print("3 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [0]) == 4:
+     print("4 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [1]) == 0:
+     print("No instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [1]) == 1:
+     print("1 instance of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [1]) == 2:
+     print("2 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [1]) == 3:
+     print("3 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [1]) == 4:
+     print("4 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+    
+if (one_letter == five_letter_word [2]) == 0:
+     print("No instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [2]) == 1:
+     print("1 instance of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [2]) == 2:
+     print("2 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [2]) == 3:
+     print("3 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [2]) == 4:
+     print("4 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [3]) == 0:
+     print("No instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [3]) == 1:
+     print("1 instance of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [3]) == 2:
+     print("2 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [3]) == 3:
+     print("3 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [3]) == 4:
+     print("4 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [4]) == 0:
+     print("No instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [4]) == 1:
+     print("1 instance of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [4]) == 2:
+     print("2 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [4]) == 3:
+     print("3 instances of " + str(one_letter) + " found in " + str(five_letter_word))
+
+if (one_letter == five_letter_word [4]) == 4:
+     print("4 instances of " + str(one_letter) + " found in " + str(five_letter_word))

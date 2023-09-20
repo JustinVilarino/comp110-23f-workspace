6e70f7f7844b8a0a00432aa4125413f738380127
Justin Vilarino <justinv@ad.unc.edu>
2023-09-19 21:56:35 -0400

    Progress on Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -5,12 +5,12 @@ __author__ = "730718451"
 secret_word: str = "python"
 six_letter_guess: str = input("What is your 6-letter guess?")
 
+while len(six_letter_guess) != 6:
+    six_letter_guess = str(input("That was not six letters! Try again:"))
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
-
-while len(six_letter_guess) != 6:
-    six_letter_guess = str(input("That was not six letters! Try again:"))
 
 while six_letter_guess != len(secret_word):
     if six_letter_guess [0] == secret_word [0]:
@@ -24,4 +24,3 @@ if six_letter_guess != secret_word:
 
 if six_letter_guess == secret_word:
     print("Woo! You got it!")
-

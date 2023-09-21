2e57e92224b79f72ad225eeaf1d6654e9549dc8b
Justin Vilarino <justinv@ad.unc.edu>
2023-09-20 21:52:46 -0400

    Progress on Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -8,15 +8,22 @@ six_letter_guess: str = input("What is your 6-letter guess?")
 while len(six_letter_guess) != 6:
     six_letter_guess = str(input("That was not six letters! Try again:"))
 
+letter_index: int = 0
+
+emoji: str = ""
+
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-while six_letter_guess != len(secret_word):
-    if six_letter_guess [0] == secret_word [0]:
-        print(GREEN_BOX)
+while letter_index < len(secret_word):
+    if six_letter_guess[letter_index] == secret_word[letter_index]:
+        letter_index = letter_index + 1
+        emoji = emoji + GREEN_BOX
     else:
-        print(WHITE_BOX)
+        letter_index = letter_index + 1
+        emoji = emoji + WHITE_BOX
+print(emoji)
 
 if six_letter_guess != secret_word:
     print("Not quite. Play again soon!")
